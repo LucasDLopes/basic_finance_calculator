17e9b017334f53a0ae460e20f6d3f185ce1d7c52
LucasDLopes <lucas@lucaslopes.com>
2025-08-29 19:09:25 -0400

    Added comments for DB store

--- backend/src/quotesStoreDB.ts
+++ backend/src/quotesStoreDB.ts
@@ -1,4 +1,3 @@
-// quotesStoreDb.ts
 import Database from "better-sqlite3";
 import { QuoteInput, QuoteResult } from "./calc.js";
 
@@ -12,6 +11,7 @@ export type SavedQuote = {
 export class QuotesStore {
   private db: Database.Database;
 
+  /* Set up database and main table */
   constructor() {
     this.db = new Database("quotes.db");
 
@@ -25,11 +25,14 @@ export class QuotesStore {
     `);
   }
 
+  /* Pull quotes from database */
   list(): SavedQuote[] {
+    //Grab rows
     const rows = this.db
       .prepare("SELECT id, input, result, quoteName FROM quotes")
       .all() as Array<{ id: string; input: string; result: string; quoteName: string }>;
 
+    //Return mapped "quoteObject" results.
     return rows.map((r) => ({
     id: r.id,
     input: JSON.parse(r.input) as QuoteInput,
@@ -39,14 +42,17 @@ export class QuotesStore {
   }
 
   add(entry: { input: QuoteInput; result: QuoteResult; name?: string }): SavedQuote {
+    //Check to see if there already exists a quote with matching information
     const exists = this.db
       .prepare("SELECT COUNT(*) as c FROM quotes WHERE input = ?")
       .get(JSON.stringify(entry.input)) as { c: number } | undefined;
 
+    //Check for match, return error if needed
     if ((exists?.c ?? 0) > 0) {
       throw new Error("A quote with identical input already exists");
     }
 
+    // passed check, create new ID for quote and insert it into the database.
     const id = Math.random().toString(36).slice(2, 10);
     const quoteName = entry.name || "Untitled Quote";
 
@@ -57,6 +63,7 @@ export class QuotesStore {
     return { id, input: entry.input, result: entry.result, quoteName };
   }
 
+  /* Take as input quote id and remove it from DB */
   remove(id: string): boolean {
     const res = this.db.prepare("DELETE FROM quotes WHERE id = ?").run(id);
     return res.changes > 0;
